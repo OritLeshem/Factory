70b988e2a5c7d340fa715a316a84ae43f9ee3292
OritLeshem <azmono@gmail.com>
2023-09-28 14:01:37 -0400

    frontend employee page, add employee page

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const { connect: connectDB } = require('./config/db');
 const departmentRouter = require('./api/department/department.routes')
 const employeeRouter = require('./api/employee/employee.routes')
+const shiftRouter = require('./api/shift/shift.routes')
 
 const app = express()
 const PORT = 5001
@@ -12,6 +13,8 @@ app.use(express.json())
 //routes
 app.use('/department', departmentRouter)
 app.use('/employee', employeeRouter)
+app.use('/shift', shiftRouter)
+
 
 app.listen(PORT, () => {
   console.log(`app is listening on port http://localhost:${PORT}
